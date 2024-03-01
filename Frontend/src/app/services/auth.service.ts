c152787820ea0395ad5104c6b379198430c2cd50
d-kingangi <itsronduncan@gmail.com>
2024-03-01 19:50:53 +0300

    karma config

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -11,7 +11,36 @@ export class AuthService {
 
   constructor(private http:HttpClient) { }
 
-  loginUser(user_details:loginDetails ){
-    return this.http.post<{message:string, token:string, error:string}>('http://localhost:4100/auth/login', user_details)
+  loginUser(details: loginDetails): Observable<{ message: string, token: string, error: string }> {
+    return this.http.post<{ message: string, token: string, error: string }>('http://localhost:4100/auth/login', details);
+  }
+
+  readToken(token: string = ''): Observable<{ info: { userId: string, firstname: string, lastname: string, email: string, content: string, password: string, isAdmin: any } }> {
+    console.log('Token:', token);
+
+    return this.http.get<{ info: { userId: string, firstname: string, lastname: string, email: string, content: string, password: string, isAdmin: any } }>('http://localhost:4100/auth/checkdetails', {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'token': token
+      })
+    }).pipe(
+      tap(response => console.log('Response:', response))
+    );
+  }
+
+  getUserDetails(token?: string): Observable<{ userId: string, firstname: string, lastname: string, email: string, isAdmin: any }> {
+    if (!token) {
+      return of({ userId: '', firstname: '', lastname: '', email: '', isAdmin: true});
+    }
+
+    return this.readToken(token).pipe(
+      map(res => ({
+        userId: res.info.userId,
+        firstname: res.info.firstname,
+        lastname: res.info.lastname,
+        email: res.info.email,
+        isAdmin: res.info.isAdmin === 'true'
+      }))
+    );
   }
 }
